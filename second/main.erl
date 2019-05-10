14332e751b7806656b8227393eb511caeda8651d
arseniy <molodeckij.b.2017@fml31.ru>
2019-05-10 04:56:53 +0300

    completed first part of creating Directed graph

---
+++ second/main.erl
@@ -0,0 +1,62 @@
+-module(main). 
+-export([start/0,create_dgraph/2,create_edge/2,
+add_edge/2,delete_edge/2,vertexes/1,edges/1,from/1,to/1, get_another_vertex/2]). 
+
+-record(edge, {from, to}).
+-record(dgraph, {vertexes, edges}).
+
+from(Edge) -> Edge#edge.from.
+to(Edge) -> Edge#edge.to.
+vertexes(DGraph) -> DGraph#dgraph.vertexes.
+edges(DGraph) -> DGraph#dgraph.edges.
+
+create_edge(From, To) -> 
+   #edge{from = From, to = To}.
+
+create_dgraph(Vertexes,Edges) ->
+   #dgraph{vertexes = Vertexes, edges = Edges}.
+
+add_edge(Edge, DGraph)->
+   case (lists:member(from(Edge),vertexes(DGraph)) 
+   and lists:member(to(Edge),vertexes(DGraph))) of 
+      true ->
+         NewEdges = lists:append(Edge, edges(DGraph)),
+         create_dgraph(vertexes(DGraph),NewEdges);
+      false -> DGraph
+   end.
+
+delete_edge(Edge, DGraph)->
+   OldEdges = edges(DGraph),
+   NewEdges = lists:delete(Edge,OldEdges),
+   case NewEdges of
+      OldEdges -> DGraph;
+      ChangedEdges -> 
+         NewDGraph = create_dgraph(vertexes(DGraph),ChangedEdges),
+         delete_edge(Edge,NewDGraph)
+   end.
+
+get_another_vertex(Vertex, Edge) ->
+   case from(Edge) of 
+         Vertex -> to(Edge);
+         AnotherVertex -> AnotherVertex
+   end.
+
+filter_by_vertex(Vertex, DGraph) ->
+   NewEdges = lists:filter(fun ([ _ | Edge]) -> 
+      (from(Edge) =:= Vertex) or (to(Edge) =:= Vertex) end,
+      edges(DGraph)),
+   NewVertexes = lists:map(fun (Edge) ->
+      get_another_vertex(Vertex, Edge)
+         end,
+      NewEdges),
+   create_dgraph(NewVertexes, NewEdges).
+
+
+
+print_int(Number) ->
+   io:fwrite(integer_to_list(Number)).
+
+start() -> 
+   Edge = create_edge(1,5), 
+   print_int(Edge#edge.to),
+   io:fwrite("~n").
