c16e05f7bd2115f39cc0035789c196c9b8396ea4
arseniy <molodeckij.b.2017@fml31.ru>
2019-06-27 23:27:20 +0300

    asderfgthyjkl

--- third/main.erl
+++ third/main.erl
@@ -1,6 +1,6 @@
 -module(main). 
 -export([main/1, listen_interpolater/0, listen_reciever/0]).
--import(interpolation,[interpolate/2]).
+-import(interpolation,[interpolate/2, create_point/2]).
 
 listen_interpolater() ->
    receive
@@ -27,25 +27,37 @@ listen_reciever() ->
    end.
 
 
-fill_start_point([Points]) ->
+fill_point(Points, Pids) ->
+   Line = io:get_line(""),
+   if 
+      (Line =:= "stop\n") -> ok;
+      true -> 
+         X_Tuple = [string:to_float(lists:droplast(Line))],
+         [{X,_}] = X_Tuple,
+         [Interpolater | Receiver] = Pids,
+         Interpolater ! {calculate, Receiver, Points, X},
+         fill_point(Points,Pids)
+   end.
+
+fill_start_point(Max_N, Max_N, Points, Pids) -> fill_point(Points, Pids);
+fill_start_point(N, Max_N, Points, Pids) ->
    Line = io:get_line(""),
    if 
       (Line =:= "stop\n") -> ok;
       true -> 
          Point_Tokens = string:tokens(Line, ";"),
-
-         
-X = 
-Y = 
-         Point = create_point(X,Y).
-         fill_start_point([Point | Points])
+         [ X_String | Y_String ] = Point_Tokens,
+         X_Tuple = [string:to_float(X_String)],
+         [{X,_}] = X_Tuple,
+         Y_Tuple = [string:to_float(lists:droplast(Y_String))],
+         [{Y,_}] = Y_Tuple,
+         Point = create_point(X,Y),
+         fill_start_point(N + 1, Max_N,[Point | Points], Pids)
    end.
 
-fill_point() ->
-   
-   ok.
 
 main(Args) ->
-   % Pidasdasd = spawn(fun listen_receiver/0)
+   Pid = spawn(fun listen_interpolater/0),
+   Pid2 = spawn(fun listen_reciever/0),
    io:format("Args: ~p\n", [Args]),
-   fill_start_point([]).
+   fill_start_point(5,6,[],[Pid,Pid2]).
